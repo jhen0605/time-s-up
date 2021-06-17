162c3f401490cc8c3f68056f5192ded557b59b19
Jhen <79475798+jhen0605@users.noreply.github.com>
2021-06-17 18:30:25 +0800

    Initial Commit

---
+++ time's up/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  time's up
+//
+//  Created by 簡吟真 on 2021/6/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
